2556c7c3a2dc52183b0efd819e80cb57daffda84
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-06-15 10:09:00 +0200

    init commit

---
+++ src/main/java/pl/sda/eprzychodnia/EPrzychodniaApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.eprzychodnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPrzychodniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EPrzychodniaApplication.class, args);
+    }
+
+}
