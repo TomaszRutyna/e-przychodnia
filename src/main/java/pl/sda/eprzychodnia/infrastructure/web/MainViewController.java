159cba4afd28beae4dee929f0537d3dcba7ba303
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-06-15 11:45:46 +0200

    pierwszy widok z danymi przekazywanymi z serwera

--- src/main/java/pl/sda/eprzychodnia/infrastructure/web/MainViewController.java
+++ src/main/java/pl/sda/eprzychodnia/infrastructure/web/MainViewController.java
@@ -2,12 +2,17 @@ package pl.sda.eprzychodnia.infrastructure.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
 
 @Controller
 class MainViewController {
 
     @GetMapping
-    String mainView() {
-        return "index.html";
+    ModelAndView mainView() {
+        ModelAndView mav = new ModelAndView("index.html");
+        mav.addObject("todayDate", LocalDate.now());
+        return mav;
     }
 }
