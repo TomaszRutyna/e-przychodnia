7d12805b27ce1092b38db2c3280030906ee364c8
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-06-16 12:55:52 +0200

    usuniecie properties dotyczacy bazy danych

--- src/main/java/pl/sda/eprzychodnia/EPrzychodniaApplication.java
+++ src/main/java/pl/sda/eprzychodnia/EPrzychodniaApplication.java
@@ -3,9 +3,13 @@ package pl.sda.eprzychodnia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@PropertySource(value = {"application.properties", "application-ext.properties"},
+        ignoreResourceNotFound = true)
 public class EPrzychodniaApplication {
 
     public static void main(String[] args) {
