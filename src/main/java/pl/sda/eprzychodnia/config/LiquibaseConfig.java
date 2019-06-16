a2409f7983fcd394f59d10395bd0f439859ba4f1
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-06-16 13:47:08 +0200

    migracje bazy danych

---
+++ src/main/java/pl/sda/eprzychodnia/config/LiquibaseConfig.java
@@ -0,0 +1,24 @@
+package pl.sda.eprzychodnia.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/liquibase-changelog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+
+}
