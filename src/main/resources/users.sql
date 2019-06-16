75954ab2146ba6c8eb5fb5cb5dac33a5d4f6d56c
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-06-16 12:46:19 +0200

    security - sterowanie wyswietleniem elementow na podstawie roli

--- src/main/resources/users.sql
+++ src/main/resources/users.sql
@@ -2,4 +2,3 @@ INSERT INTO user_roles (id, role) VALUES (1, 'ROLE_USER');
 INSERT INTO users (id, login, password) VALUES (1, 'user', '$2a$10$iNWL3dCpWChiBQGU6HAUxOs6iq6ev2nOk6TqlG1T24rX7DFdDvpi.');
 INSERT INTO users_roles (user_id, roles_id) VALUES (1, 1);
 
-
