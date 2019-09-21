c9c9cbed68ea4f1461ebe03244b4e53b16eadb93
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-09-21 13:56:28 +0200

    Docs

--- src/main/java/pl/sda/eprzychodnia/infrastructure/client/nbp/ExchangeRateClient.java
+++ src/main/java/pl/sda/eprzychodnia/infrastructure/client/nbp/ExchangeRateClient.java
@@ -6,15 +6,32 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 
+/**
+ * Klasa sluzaca do pobierania danych o walutach z NBP
+ *
+ * @author trutyna
+ * @since 20-09-2019
+ */
 @Component
 @RequiredArgsConstructor
 public class ExchangeRateClient {
     private final RestTemplate restTemplate;
 
-    public void getExchangeRate(LocalDate forDate) {
+    /**
+     * Metoda pobiera kursy walut dla podanej daty
+     *
+     * @see ExchangeRateTable
+     *
+     * @param forDate dla podanej daty
+     * @throws NullPointerException gdy podana data jest nullem
+     * @return Nazwa tabeli z NBP
+     */
+    public String getExchangeRate(LocalDate forDate) {
         ExchangeRateTable table = restTemplate.getForEntity("https://www.nbp.pl/kursy/xml/a119z190621.xml", ExchangeRateTable.class).getBody();
 
         System.out.println(table.getTableNumber());
+
+        return table.getTableNumber();
     }
 
 }
